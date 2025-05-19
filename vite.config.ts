698e214331fe78689ec89dce2219bf23092e72c6
oniafk <manufelipearias@gmail.com>
2025-05-20 00:24:33 +1000

    set up deployment vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: './',
+  base: "/",
   build: {
-    outDir: 'dist'
-  }
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ganadores: resolve(__dirname, "ganadores.html"),
+      },
+    },
+  },
 });
